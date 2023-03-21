8b24e3495997e3c83b5ccd13edc547a61f850eee
Alhafat <vladimir-lukin@inbox.ru>
2023-03-21 23:20:31 +0300

    '20/03/2023'

--- seminars/seminar_4/lesson/exp_1.py
+++ seminars/seminar_4/lesson/exp_1.py
@@ -8,13 +8,10 @@ Input: a a a b c a a d c d d
 Output: a a _1 a_2 b c a_3 a_4 d c_1 d_1 d_2
 """
 
-text = ['a', 'a', 'a', 'b', 'c', 'a', 'a', 'd', 'c', 'd', 'd']
-    # input('Введите строку: ').split()
+text = input('Введите строку: ').split()
 print(text)
 for i in range(len(text))[::-1]:
     count = text[:i].count(text[i])
-    print(count)
     if count > 0:
-        text[i] = text[i] + '_' + str(count)
-        # print(count)
+        text[i] +=  + '_' + str(count)
 print(text)
