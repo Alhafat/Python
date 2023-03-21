e6d0c876ea7495b2f98f992c2ea1d45eec0e64c2
Alhafat <vladimir-lukin@inbox.ru>
2023-03-21 22:08:42 +0300

    '20/03/2023'

--- seminars/seminar_4/lesson/exp_1.py
+++ seminars/seminar_4/lesson/exp_1.py
@@ -5,13 +5,16 @@
 уже встречался. Количество повторов добавляется к
 символам с помощью постфикса формата _n.
 Input: a a a b c a a d c d d
-Output: a a_1 a_2 b c a_3 a_4 d c_1 d_1 d_2
+Output: a a _1 a_2 b c a_3 a_4 d c_1 d_1 d_2
 """
 
-text = sorted(input('Введите строку: '))
+text = ['a', 'a', 'a', 'b', 'c', 'a', 'a', 'd', 'c', 'd', 'd']
+    # input('Введите строку: ').split()
 print(text)
-for i in range(len(text)):
+for i in range(len(text))[::-1]:
     count = text[:i].count(text[i])
+    print(count)
     if count > 0:
-        text[i] += '_' + str(count)
+        text[i] = text[i] + '_' + str(count)
+        # print(count)
 print(text)
