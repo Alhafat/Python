bbb284a54476ab7d4ef50822038cd1d610147d83
Alhafat <vladimir-lukin@inbox.ru>
2023-03-28 15:37:14 +0300

    '28/03/2023'

--- seminars/seminar_5/homework/task_2.py
+++ seminars/seminar_5/homework/task_2.py
@@ -9,3 +9,19 @@
 """
 
 
+def get_summ_numbers(a, b, summ_numbers=0):
+    if a == 0 and b == 0:
+        return summ_numbers
+    else:
+        summ_numbers += 2
+        return get_summ_numbers(a - 1, b - 1, summ_numbers)
+
+
+def main():
+    a = int(input('Введите первое число: '))
+    b = int(input('Введите второе число: '))
+    print(get_summ_numbers(a, b))
+
+
+if __name__ == '__main__':
+    main()
