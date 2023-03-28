310cf5ffe5a8cbef904aa24914e93c9c9ea8ec49
Alhafat <vladimir-lukin@inbox.ru>
2023-03-28 22:47:49 +0300

    '28/03/2023'

--- seminars/seminar_6/lesson/exp_3.py
+++ seminars/seminar_6/lesson/exp_3.py
@@ -10,37 +10,36 @@
 Вывод:
 2
 """
+
 from random import randint
 
+numbers = [randint(0, 10) for _ in range(10)]
+print(sorted(numbers))
+numbers_1 = set(numbers)
+# print(numbers_1)
+temp = []
+for i in numbers_1:
+    if numbers.count(i) > 1:
+        temp.append(numbers.count(i) // 2)
+# print(temp)
+print(sum(temp))
 
-# numbers = [randint(0, 10) for _ in range(10)]
-# print(sorted(numbers))
-# numbers_1=set(numbers)
-# # print(numbers_1)
-# temp=[]
-# for i in numbers_1:
-#     if numbers.count(i)>1:
-#         temp.append(numbers.count(i)//2)
-# # print(temp)
-# print(sum(temp))
-
-
-def input_num(message: str) -> int:
-    input_error: bool = True
-    while input_error:
-        try:
-            temp = int(input(message))
-        except ValueError:
-            print("Вы ввели не число!")
-        else:
-            input_error = False
-            return temp
-
-
-# n = [randint(1, 6) for i in range(input_num('Please input size of N list: '))]
-n = [1, 2, 3, 2, 3]
-print(n)
-l_set = set(n)
-for x in l_set:
-    if n.count(x) - 1:
-        print(f' digit {x}, count - {(n.count(x) - 1) * (n.count(x)) / 2}', end=" //  ")
+# def input_num(message: str) -> int:
+#     input_error: bool = True
+#     while input_error:
+#         try:
+#             temp = int(input(message))
+#         except ValueError:
+#             print("Вы ввели не число!")
+#         else:
+#             input_error = False
+#             return temp
+#
+#
+# # n = [randint(1, 6) for i in range(input_num('Please input size of N list: '))]
+# n = [1, 2, 3, 2, 3]
+# print(n)
+# l_set = set(n)
+# for x in l_set:
+#     if n.count(x) - 1:
+#         print(f' digit {x}, count - {(n.count(x) - 1) * (n.count(x)) / 2}', end=" //  ")
