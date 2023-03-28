ceb2b97023f63da9481efacc0e5ed0cb91c02e1a
Alhafat <vladimir-lukin@inbox.ru>
2023-03-28 16:40:14 +0300

    '28/03/2023'

--- seminars/seminar_5/homework/task_2.py
+++ seminars/seminar_5/homework/task_2.py
@@ -12,12 +12,13 @@
 def get_summ_numbers(a, b, summ_numbers=0):
     if a == 0 and b == 0:
         return summ_numbers
-    elif a!=0 and b!=0:
+    elif a != 0 and b != 0:
         summ_numbers += 2
-        return get_summ_numbers(a-1 if a>0 else 0,b-1 if b>0 else 0, summ_numbers)
+        return get_summ_numbers(a - 1 if a > 0 else 0, b - 1 if b > 0 else 0, summ_numbers)
     else:
         summ_numbers += 1
         return get_summ_numbers(a - 1 if a > 0 else 0, b - 1 if b > 0 else 0, summ_numbers)
+
 
 def main():
     a = int(input('Введите первое число: '))
