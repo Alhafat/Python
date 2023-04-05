0314f90cd3d6f2e4a0a6133ddd8c46f19a639f4e
Alhafat <vladimir-lukin@inbox.ru>
2023-04-05 15:41:00 +0300

    '2023-04'

--- exp/any_exp/any.py
+++ exp/any_exp/any.py
@@ -67,6 +67,14 @@ from re import sub
 # s=list(filter(lambda x: type(x)==int, l))
 # print(s)
 
-solution([-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20])
-# returns "-10--8,-6,-3-1,3-5,7-11,14,15,17-20"
 
+# solution([-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20])
+# # returns "-10--8,-6,-3-1,3-5,7-11,14,15,17-20"
+
+
+# arr = [i for i in range(20, 241)]
+# test=[1 if i % 20 == 0 else 0 for i in arr if i % 20 == 0 or i % 21 == 0]
+# print(f'делится на 20 -> {test.count(1)}, делится на 21 -> {test.count(0)}')
+
+
+# print(len([i for i in range(len(arr)) if i % 20 == 0 or i % 21 == 0]))
